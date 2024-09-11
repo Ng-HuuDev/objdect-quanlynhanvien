685107a01b65a239070e6cd1f70e1646b3bc6ddb
Ng-HuuDev <nguyenhuudev0203@gmail.com>
2024-09-12 02:23:15 +0700

    ad

---
+++ js/controller.js
@@ -0,0 +1,56 @@
+// tao ham renderDSNV
+
+function renderDSNV(dsnv, NV) {
+  var contentHtml = "";
+  for (var index = 0; index < dsnv.length; index++) {
+    NV = dsnv[index];
+    var trString = `
+      <tr>
+      <td> ${NV.tk}</td>
+      <td> ${NV.ten}</td>
+      <td> ${NV.email}</td>
+      <td> ${NV.ngayLam}</td>
+      <td> ${NV.chucVu}</td>
+      <td> 0</td>
+      <td> 0</td>
+      <td> 
+      <button  onclick=xoaNV("${NV.tk}")  class ="btn btn-success"> Xóa </button>
+      <button onclick=suaNV("${NV.tk}") class= "btn btn-danger"> Sửa </button>
+      </td>
+      </tr>
+      `;
+
+    contentHtml += trString;
+  }
+  // duyet mang = forEach
+  // DSNV.forEach(function (nv, index) {
+  //     nv = dsnv[index];
+  //     var trString = `
+  //     <tr>
+  //     <td> ${nv.tk}</td>
+  //     <td> ${nv.ten}</td>
+  //     <td> ${nv.email}</td>
+  //     <td> ${nv.ngayLam}</td>
+  //     <td> ${nv.chucVu}</td>
+  //     <td> 0</td>
+  //     <td> 0</td>
+  //     </tr>
+  //     `;
+  //     contentHtml += trString;
+  //   });
+
+  document.getElementById("tableDanhSach").innerHTML = contentHtml;
+}
+
+// viet ham hienThiThongTin
+
+function hienThiThongTin(nv) {
+  document.getElementById("tknv").value = nv.tk;
+  document.getElementById("name").value = nv.ten;
+  document.getElementById("email").value = nv.email;
+  document.getElementById("password").value = nv.matKhau;
+  document.getElementById("datepicker").value = nv.ngayLam;
+  document.getElementById("luongCB").value = nv.luongCB;
+  document.getElementById("chucvu").value = nv.chucVu;
+  document.getElementById("gioLam").value = nv.giolam;
+}
---
+++ js/index.js
@@ -0,0 +1,69 @@
+// tao mang chua DSNV
+
+var DSNV = [];
+
+// them nv
+
+function themnv() {
+  // nhap thong tin nguoi dung
+  // tao object
+  //  push nv
+  // render ra dsnv
+  var tk = document.getElementById("tknv").value;
+  var ten = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var matKhau = document.getElementById("password").value;
+  var ngayLam = document.getElementById("datepicker").value;
+  var luongCB = document.getElementById("luongCB").value;
+  var chucVu = document.getElementById("chucvu").value;
+  var giolam = document.getElementById("gioLam").value;
+
+  // tao object
+  var nv = {
+    tk,
+    ten,
+    email,
+    matKhau,
+    ngayLam,
+    luongCB,
+    chucVu,
+    giolam,
+    // tinhLuong: function () {
+    //   var tongLuong = 0;
+    // },
+  };
+  // push nv
+  DSNV.push(nv);
+
+  // render DSNV
+
+  renderDSNV(DSNV);
+}
+
+// xoa nv
+function xoaNV(id) {
+  // tim vi tri- index cua nhan can xoa => findIdex
+  var index = DSNV.findIndex(function (item) {
+    return item.tk == id;
+  });
+
+  // su dung splice(vitri, so luong xoa)
+  DSNV.splice(index, 1);
+
+  // cap nhat layout sau khi xoa
+  renderDSNV(DSNV);
+}
+
+// sua Sv
+
+function suaNV(id) {
+  // tim vi tri- index cua nhan can xoa => findIdex
+  var index = DSNV.findIndex(function (item) {
+    return item.tk == id;
+  });
+
+  var nv = DSNV[index];
+
+  // hien thi thong tin tren form
+  hienThiThongTin(nv);
+}
